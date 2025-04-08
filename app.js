bb4f580b023b49aa658ef75c1f7d53bec65a46db
Dito Barata Junanto <ditobarata@gmail.com>
2025-04-08 22:20:26 +0700

    commit pertama

---
+++ app.js
@@ -0,0 +1,25 @@
+// Koneksi ke broker MQTT over WebSocket
+const broker = "wss://test.mosquitto.org:8081";
+const topic = "chandanata/tes";
+
+// Buat koneksi client MQTT
+const client = mqtt.connect(broker);
+
+const messagesDiv = document.getElementById("messages");
+
+// Saat konek sukses
+client.on("connect", () => {
+  console.log("🔌 Sambung menyang broker MQTT");
+  client.subscribe(topic, (err) => {
+    if (!err) {
+      console.log("📡 Ngleboni topik:", topic);
+    }
+  });
+});
+
+// Saat nerima pesan
+client.on("message", (topic, message) => {
+  const msg = message.toString();
+  console.log(`📥 [${topic}]: ${msg}`);
+  messagesDiv.innerHTML += `<div><strong>${topic}:</strong> ${msg}</div>`;
+});
